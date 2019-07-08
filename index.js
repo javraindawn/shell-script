1d683ee7cb88bebf299675dfcb4f925c2ed21a74
Joseph Angelo Rendon <joseph@activelamp.com>
2019-07-08 14:25:34 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var shell = require('shelljs');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/webhook/deploy/iloveyou3000", function(req, res) {
+    shell.exec('./deploy-speed-debug.sh');
+    res.status(200).send("thanks");
+});
+
+var server = app.listen(80, function () {
+    console.log("app running on port.", server.address().port);
+});
