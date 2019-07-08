c803dbe9e6c4bba420fd41a9d907fc751a6bbcb0
Joseph Angelo Rendon <joseph@activelamp.com>
2019-07-08 14:37:04 +0800

    8069

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get("/webhook/deploy/iloveyou3000", function(req, res) {
     res.status(200).send("thanks");
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(8069, function () {
     console.log("app running on port.", server.address().port);
 });
